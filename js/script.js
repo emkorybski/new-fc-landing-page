dad789657cc030377d34fe97c8cba1e9a31338b4
emkorybski <emkorybski@gmail.com>
2015-03-22 12:49:02 +0000

    All text added and formatted.

--- js/script.js
+++ js/script.js
@@ -2,7 +2,19 @@
 
 */
 
-
+$(document).ready(function(){
+    $("#toggle-terms").bind("click", function() {
+        console.log("CLICKED");
+        if ($("#terms").hasClass("content-inactive")){
+            $("#terms").removeClass("content-inactive");
+            $("#terms").addClass("content-active");
+        }
+        else if ($("#terms").hasClass("content-active")){
+            $("#terms").removeClass("content-active");
+            $("#terms").addClass("content-inactive");
+        }
+    });
+});
 
 
 

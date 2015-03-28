f54d22bf200fe8f65d7b2212c760ff55a5e7ae6e
emkorybski <emkorybski@gmail.com>
2015-03-28 13:50:40 +0000

    RADR removed, T&Cs display in overlay box

--- js/script.js
+++ js/script.js
@@ -4,14 +4,26 @@
 
 $(document).ready(function(){
     $("#toggle-terms").bind("click", function() {
-        console.log("CLICKED");
+        //console.log("CLICKED");
         if ($("#terms").hasClass("content-inactive")){
             $("#terms").removeClass("content-inactive");
             $("#terms").addClass("content-active");
+            $(this).hide();
         }
-        else if ($("#terms").hasClass("content-active")){
+        /*
+         else if ($("#terms").hasClass("content-active")){
+         $("#terms").removeClass("content-active");
+         $("#terms").addClass("content-inactive");
+         }
+         */
+    });
+    $("#close-terms").bind("click", function() {
+        //console.log("CLICKED");
+        if ($("#terms").hasClass("content-active")){
             $("#terms").removeClass("content-active");
             $("#terms").addClass("content-inactive");
+            $(this).parent().hide();
+            $("#toggle-terms").show();
         }
     });
 });
